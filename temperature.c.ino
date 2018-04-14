b398199e3b04d421d6c02546b86bebacd81f4367
skhan72 <37481389+skhan72@users.noreply.github.com>
2018-04-14 11:41:31 -0500

    Update temperature.c.ino

--- temperature.c.ino
+++ temperature.c.ino
@@ -1,7 +1,11 @@
 #include <LiquidCrystal.h>
 
+ int LED3 = 9;
+ int LED2= 13;
+ int LED1 = 10;
 int ThermistorPin = 0;
 int Vo;
+int brightness = 100;
 float R1 = 10000;
 float logR2, R2, T;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
@@ -10,6 +14,10 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
 Serial.begin(9600);
+pinMode (LED3, OUTPUT);
+pinMode (LED2, OUTPUT);
+pinMode (LED1, OUTPUT);
+
 }
 
 void loop() {
@@ -25,7 +33,26 @@ void loop() {
   lcd.print(T);   
   lcd.print(" F");
   
+  if (T < 32) {
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED3, HIGH);
+  } 
+   if (T >= 32 && T < 80){
+    digitalWrite (LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+
+  }
+  else {
+    digitalWrite (LED3, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED1, HIGH);
+  }
+  
   delay(500);            
   lcd.clear();
+
 }
+
 
